2eba63a9809d5bf61c3bf6985748948a211007dd
Laoliveira27 <rm567653@fiap.com.br>
2025-11-20 19:39:09 -0300

    validação do formulário de contato com ajuste  visual

--- js/main.js
+++ js/main.js
@@ -35,3 +35,40 @@ if (formDiario) {
     humorButtons.forEach(b => b.classList.remove('selecionado'));
   });
 }
+
+
+const formContato = document.getElementById('contact-form');
+const feedbackContato = document.getElementById('form-feedback');
+
+if (formContato) {
+  formContato.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome');
+    const email = document.getElementById('email');
+    const tipo = document.getElementById('tipo');
+    const mensagem = document.getElementById('mensagem');
+
+    if (!nome.value.trim() || !email.value.trim() || !tipo.value || !mensagem.value.trim()) {
+      feedbackContato.style.color = '#b12020';
+      feedbackContato.textContent = "Por favor, preencha todos os campos obrigatórios!";
+      return;
+    }
+
+    const emailValido = /\S+@\S+\.\S+/.test(email.value);
+    if (!emailValido) {
+      feedbackContato.style.color = '#b12020';
+      feedbackContato.textContent = "Digite um e-mail válido!";
+      return;
+    }
+ 
+    feedbackContato.style.color = '#0c4e39';
+    feedbackContato.textContent = "Mensagem enviada com sucesso! (simulação)";
+
+
+    nome.value = "";
+    email.value = "";
+    tipo.value = "";
+    mensagem.value = "";
+  });
+}
